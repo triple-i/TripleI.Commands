66b6d06c2ee661e1f80f6451da1465d42663da6d
Skobayashi <suguru@iii-planning.com>
2014-12-11 13:30:22 +0900

    Symfonyコンポーネント化

--- src/TripleI/Command/Generate.php
+++ src/TripleI/Command/Generate.php
@@ -13,6 +13,18 @@ class Generate extends Command
 {
 
     /**
+     * @var string
+     **/
+    private $c_name;
+
+
+    /**
+     * @var string
+     **/
+    private $c_path;
+
+
+    /**
      * @return void
      **/
     protected function configure ()
@@ -34,23 +46,37 @@ class Generate extends Command
      **/
     protected function execute (InputInterface $input, OutputInterface $output)
     {
-        $command_name = ucfirst($input->getArgument('command_name'));
-        $command_path = sprintf(SRC.'/TripleI/Command/%s.php', $command_name);
+        $this->c_name = ucfirst($input->getArgument('command_name'));
+        $this->c_path = sprintf(SRC.'/TripleI/Command/%s.php', $this->c_name);
 
-        $this->_validateCommand($command_path);
+        $this->_validateCommand();
+        $command_data = $this->_buildCommandData();
+        file_put_contents($this->c_path, $command_data);
+
+        $output->writeln(sprintf('<info>Generated %s command!</info>', $this->c_name));
     }
 
 
     /**
-     * @param  string $command_path
      * @return void
      **/
-    private function _validateCommand ($command_path)
+    private function _validateCommand ()
     {
-        if (file_exists($command_path)) {
-            $info = pathinfo($command_path);
-            throw new \RuntimeException(sprintf('既に%sコマンドは存在しています'));
+        if (file_exists($this->c_path)) {
+            throw new \RuntimeException(sprintf('既に%sコマンドは存在しています', $this->c_name));
         }
+    }
+
+
+    /**
+     * @return string
+     **/
+    private function _buildCommandData ()
+    {
+        $skeleton = file_get_contents(ROOT.'/data/skeleton/CommandSkeleton.php');
+        $skeleton = str_replace('${name}', $this->c_name, $skeleton);
+
+        return $skeleton;
     }
 }
 
---
+++ tests/TripleI/Command/GenerateTest.php
@@ -0,0 +1,108 @@
+<?php
+
+
+use TripleI\Command\Generate;
+use TripleI\Console\TripleIApplication;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class GenerateTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var TripleIApplication
+     **/
+    private $app;
+
+
+    /**
+     * コマンドの名称
+
+     * @var string
+     **/
+    private $c_name;
+
+
+    /**
+     * コマンドクラスへのパス
+     *
+     * @var string
+     **/
+    private $c_path;
+
+
+    /**
+     * @return void
+     **/
+    public function setUp ()
+    {
+        $this->app = new TripleIApplication();
+        $this->app->add(new Generate());
+    }
+
+
+    /**
+     * @return void
+     **/
+    public function tearDown ()
+    {
+        if (file_exists($this->c_path)) unlink($this->c_path);
+    }
+
+
+    /**
+     * @test
+     * @expectedException          RuntimeException
+     * @expectedExceptionMessageRegExp   /既に[0-9a-zA-Z]{5}コマンドが存在しています/
+     * @group generate-already-exists-command
+     * @group generate
+     **/
+    public function 指定コマンドが既に存在している場合 ()
+    {
+        $this->_generateCommandName();
+        file_put_contents($this->c_path, '');
+
+        $command = $this->app->find('generate');
+        $tester  = new CommandTester($command);
+        $tester->execute(array(
+            'command' => $command->getName(),
+            'command_name' => $this->c_name
+        ));
+    }
+
+
+    /**
+     * @test
+     * @group generate-execute
+     * @group generate
+     **/
+    public function 正常な処理 ()
+    {
+        $this->_generateCommandName();
+
+        $command = $this->app->find('generate');
+        $tester  = new CommandTester($command);
+        $tester->execute(array(
+            'command'  => $command->getName(),
+            'command_name' => $this->c_name
+        ));
+
+        $this->assertTrue(file_exists($this->c_path));
+        $this->assertEquals(
+            sprintf('Generated %s command!'.PHP_EOL, $this->c_name),
+            $tester->getDisplay()
+        );
+    }
+
+
+    /**
+     * テスト用のコマンドファイル名を生成する
+     *
+     * @return void
+     **/
+    private function _generateCommandName ()
+    {
+        $this->c_name = ucfirst(substr(md5(uniqid()), 0, 5));
+        $this->c_path = sprintf(SRC.'/TripleI/Command/%s.php', $this->c_name);
+    }
+}
+
